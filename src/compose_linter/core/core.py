88c81006c14414b9b8e1ac879d99e60bf2b24cdc
Alex-Ashworth <git@alex-ashworth.com>
2026-07-08 23:41:21 -0500

    reworked for one loop that performs all checks

--- src/compose_linter/core/core.py
+++ src/compose_linter/core/core.py
@@ -3,53 +3,38 @@ import sys
 from pathlib import Path
 from compose_linter.utils import yaml, open_yaml
 
-compose_path = Path(__file__).with_name("compose.yml")
+compose_path = Path(__file__).with_name("compose_.yml")
 compose_dict = open_yaml(compose_path)
-services_dict = compose_dict.get("services", {})
+service_dict = compose_dict.get("services", {})
 
 def check_services_entry(compose_dict: dict):
-    if "services" not in compose_dict:
-        print("ERROR: No services detected")
-        # have this check what IS in the file and suggest/correct
-        # raise an error here? - prob so
-        # can't think of how to continue without services as a basis, couldn't be 100% certain that im accessing the correct data
-    else:
-        return
+    has_service = "services" in compose_dict
+    if not has_service:
+        print(f"ERROR: Compose file {compose_path} does not have services defined")
+        raise InvalidComposeError
 
+def iterate_services(service_dict: dict):
+    for service_name, service_data in service_dict.items():
+        if not isinstance(service_data, dict):
+            continue
 
-def get_service_names(services_dict: dict):
-    service_names = []
-    for service_name, service_data in services_dict.items():
-        service_names.append(service_name)
-    
-    return service_names
+        yield service_name, service_data
 
-def check_restart_policy(services_dict: dict):
-    for service_name, service_data in services_dict.items():
-        if isinstance(service_data, dict):
-            restart_policy = service_data.get("restart")
-            # Do something with the data
-            yaml.dump(restart_policy, sys.stdout)
-            print(type(restart_policy))
-        else:
-            restart_policy = None
-            yaml.dump(restart_policy, sys.stdout)
-            
-        if restart_policy is None:
-            print(f"ERROR: No restart policy detected for", service_name)
+def check_restart_policy(service_name, service_data):
+    has_restart = "restart" in service_data
+    if not has_restart:
+        print(f"ERROR: No restart policy was found in {service_name}")
 
 
 
-def check_image_entry(services_dict) -> bool:
-    service_names = get_service_names(services_dict)
-    for names in service_names:
-        service_data = services_dict[names]
-        images = service_data.get("image", {})
-        if not images:
-            print(f"ERROR: No image is defined for", names)
-        else:
-            return True
-   # If service has image:
+
+def check_service_source(service_name, service_data):
+    has_image = "image" in service_data
+    has_build = "image" in service_data
+    if not has_image and not has_build:
+        print(f"ERROR: No image or build entry found in {service_name}")
+
+# If service has image:
    #     optionally validate image against registry
    #        could have edge cases where docker registry is down, individial links are deprecated, etc..
 
@@ -59,8 +44,20 @@ def check_image_entry(services_dict) -> bool:
    # If service has neither image nor build:
    #     error/warning: service has no image source
 
+def check_privilege(service_name, service_data):
+    has_privilege = "privileged" in service_data
+    if has_privilege:
+        print(f"WARNING: {service_name} is running in privileged mode")
+
+def run_checks(service_dict):
+    for service_name, service_data in iterate_services(service_dict):
+        check_service_source(service_name, service_data)
+        check_privilege(service_name, service_data)
+        check_restart_policy(service_name, service_data)
+
 def run():
-    check_image_entry(services_dict)
+    check_services_entry(compose_dict)
+    run_checks(service_dict)
 
 if __name__ == "__main__":
     run()
@@ -68,4 +65,9 @@ if __name__ == "__main__":
 
 # Using .get() returns None when the restart key is missing instead of raising an error.
 #
-# TODO: check if services: exists, 
+# TODO: check for privileged mode
+#
+# TODO: learn what is going on with build: entries and handle them in the image check
+#
+# TODO: figure out how to integrate with CLI -> docker-compose-sec-linter /path/to/compose.yml
+#           argparse will likely be better for learning, Click library is the alt
