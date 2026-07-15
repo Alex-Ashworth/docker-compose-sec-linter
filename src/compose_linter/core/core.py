b11daba47d20faa22bb96e340b98b143d10b3152
Alex-Ashworth <git@alex-ashworth.com>
2026-07-14 20:19:28 -0500

    type hints

--- src/compose_linter/core/core.py
+++ src/compose_linter/core/core.py
@@ -9,30 +9,34 @@ compose_dict = open_yaml(compose_path)
 service_dict = compose_dict.get('services', {})
 container_name = service_dict.get('container_name', {})
 
+
 def check_services_entry(compose_dict: dict):
+    # check if services are defined
     has_service = 'services' in compose_dict
     if not has_service:
         print(f'{error_text('ERROR:')} Compose file {compose_path} does not have services defined')
         raise InvalidComposeError
 
 def iterate_services(service_dict: dict):
+    # helper to iterate over services 
     for service_name, service_data in service_dict.items():
         if not isinstance(service_data, dict):
             continue
 
         yield service_name, service_data
 
-def check_restart_policy(service_name, service_data):
+def check_restart_policy(service_name: str, service_data: dict):
+    # check if restart policy is defined and enabled
     has_restart = 'restart' in service_data
     if not has_restart:
         print(f'{error_text('ERROR:')} No restart policy was found in {service_name}')
+    # add logic for defined, but disabled policy
 
 
-
-
-def check_service_source(service_name, service_data):
+def check_service_source(service_name: str, service_data: dict):
+    # check if image/build is valid
     has_image = 'image' in service_data
-    has_build = 'image' in service_data
+    has_build = 'build' in service_data
     if not has_image and not has_build:
         print(f'{error_text('ERROR:')} No image or build entry found in {service_name}')
 
@@ -46,12 +50,14 @@ def check_service_source(service_name, service_data):
    # If service has neither image nor build:
    #     error/warning: service has no image source
 
-def check_privilege(service_name, service_data):
+def check_privilege(service_name: str, service_data: dict):
+    # check if container is running in priveleged mode
     has_privilege = 'privileged' in service_data
     if has_privilege:
         print(f'{warning_text('WARNING:')} {service_name} is running in privileged mode')
 
-def check_writeable(service_name, service_data):
+def check_writeable(service_name: str, service_data: dict):
+    # check if volumes are writable to the host
     volumes = service_data.get('volumes', [])
     if not volumes:
         print(f'No volumes found under {service_name}')
@@ -63,16 +69,14 @@ def check_writeable(service_name, service_data):
         else:
             options = mode.split(',')
         # handle long read_only syntax
-        print(source)
-        print(target)
-        print(mode)
         if 'ro' in options:
             print(f'{volume_entry} is read-only')
         else:
             print(f'{warning_text('WARNING:')} {highlight_text(service_name)} is writing data from the container\'s {info_text(source)} to the host\'s {info_text(target)}')
             # TODO: source & target
 
-def parse_volume(volume_entry):
+def parse_volume(volume_entry: str):
+    # split volume entries
     sections = volume_entry.split(':')
     if len(sections) == 2:
         source, target = sections
@@ -84,7 +88,7 @@ def parse_volume(volume_entry):
         return None, None, None
     return source, target, mode
 
-def run_checks(service_dict):
+def run_checks(service_dict: dict):
     for service_name, service_data in iterate_services(service_dict):
         print(f'{info_text('Scanning Service:')} {highlight_text(f'{service_name}')}')
         check_service_source(service_name, service_data)
@@ -99,6 +103,12 @@ def run():
 if __name__ == '__main__':
     run()
 
+# service_name = str
+# service_data = dict
+# service_dict = dict
+# volume_entry = str
+# source, target, mode = str
+# options = list
 
 # Using .get() returns None when the restart key is missing instead of raising an error.
 #
